8d649d7aafe43c117e50f4aee9e32facaa795618
LianQianChao <i@lchao.cc>
2021-03-10 17:29:34 +0800

    @Mapper

--- src/main/java/org/app/speed/SpeedApplication.java
+++ src/main/java/org/app/speed/SpeedApplication.java
@@ -1,9 +1,11 @@
 package org.app.speed;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.app.speed.mapper")
 public class SpeedApplication {
 
 	public static void main(String[] args) {
